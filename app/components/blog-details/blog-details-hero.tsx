e3760d204ed4493edf8827955d82b0b20ab40417
vasudevyadav <yadavvasudev28@gmail.com>
2026-03-25 17:55:40 +0530

    chnages  done

--- app/components/blog-details/blog-details-hero.tsx
+++ app/components/blog-details/blog-details-hero.tsx
@@ -12,7 +12,7 @@ type BlogDetailsHeroProps = {
 export default function BlogDetailsHero({ hero }: BlogDetailsHeroProps) {
   return (
     <div className="lg:my-12 my-6 lg:mx-24 mx-6 lg:mt-40 mt-36">
-      <section className="relative z-0 lg:h-[200px] h-[100px] w-full overflow-hidden rounded-3xl">
+      <section className="relative z-0 lg:h-[500px] object-cover h-[100px] w-full overflow-hidden rounded-3xl">
         <Image
           src={hero.image}
           alt={hero.title}
@@ -21,12 +21,12 @@ export default function BlogDetailsHero({ hero }: BlogDetailsHeroProps) {
           className="object-cover"
         />
 
-        <div className="absolute inset-0 bg-black/20" />
+        <div className="absolute inset-0 bg-black/10" />
 
         <div className="absolute inset-0 z-20 flex flex-col items-center justify-center text-center px-6">
-          <h1 className="text-white text-xl lg:text-4xl font-semibold">
+          {/* <h1 className="text-white text-xl lg:text-4xl font-semibold">
             {hero.title}
-          </h1>
+          </h1> */}
         </div>
       </section>
     </div>
